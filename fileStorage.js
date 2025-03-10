3cb80ef025d644c931dd2fab09eecada79c73400
Hit1ger <141906709+Hit1ger@users.noreply.github.com>
2025-03-10 20:20:46 +0500

    Update fileStorage.js

--- fileStorage.js
+++ fileStorage.js
@@ -93,4 +93,21 @@ document.getElementById("fetchExercisesFromServer").addEventListener("click", ()
         });
 });
 
-
+// Функция для загрузки JSON «tips» с сервера
+document.getElementById("fetchTipsFromServer").addEventListener("click", () => {
+    fetch("https://raw.githubusercontent.com/Hit1ger/gymapp/main/db/tips.json")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Ошибка загрузки: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            localStorage.setItem("tips", JSON.stringify(data));
+            alert("JSON с советами загружен успешно с сервера!");
+        })
+        .catch(error => {
+            alert("Не удалось загрузить JSON с сервера. Подробности в консоли.");
+            console.error(error);
+        });
+});
